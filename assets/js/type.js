256c876d8bf613386ed6ce435cb89dc2d94f534d
Venkatesh S <venkateshvenkatesh90377@gmail.com>
2024-03-21 16:48:30 +0530

    type

--- assets/js/type.js
+++ assets/js/type.js
@@ -1,5 +1,5 @@
 window.ityped.init(document.querySelector('.ityped'),{
-    strings: [' SOUL'],
+    strings: [' SOUL','LIFE','SPRIT','MIND'],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
